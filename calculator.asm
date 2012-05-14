1d43e7730ea6bfa4631e1255e4b75085b59a047f
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2012-05-14 22:07:51 +0300

    corrected bnf

--- calculator.asm
+++ calculator.asm
@@ -19,9 +19,9 @@
 
 # BNF for calculator:
 #
-# calculation ::= term("+"|"-" term)*
-# term ::= number("*"|"/" number)*
-# number ::= ("0"|"1"|"2"|"3"|"4"|"5"|"6"|"7"|"8"|"9")+ | "(" calculation ")"
+# <calculation> ::= <term> ("+"|"-" <term>)*
+# <term> ::= <number> ("*"|"/" <number>)*
+# <number> ::= ("0"|"1"|"2"|"3"|"4"|"5"|"6"|"7"|"8"|"9")+ | "(" <calculation> ")"
 
 # Globals:
 # $s0 = Address to memory where operation string is located. Reading "cursor".
--- readme.md
+++ readme.md
@@ -7,6 +7,6 @@
 
 ##BNF for calculator:
 
-    calculation ::= term("+"|"-" term)*
-    term ::= number("*"|"/" number)*
-    number ::= ("0"|"1"|"2"|"3"|"4"|"5"|"6"|"7"|"8"|"9")+ | "(" calculation ")"
+    <calculation> ::= <term> ("+"|"-" <term>)*
+    <term> ::= <number> ("*"|"/" <number>)*
+    <number> ::= ("0"|"1"|"2"|"3"|"4"|"5"|"6"|"7"|"8"|"9")+ | "(" <calculation> ")"
